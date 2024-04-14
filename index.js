144704ebafade91943842f1f552dcaeb73ad43aa
inigoruizdiaz <inigoruiz@MacBook-Pro-de-Inigo.local>
2024-04-14 18:09:30 +0200

    Subida de todo

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-const express = require('express');
-const app = express();
-
-
-app.use(express.static(_dirname + '/public'));
-app.listen(3000, () => {
-console.log('Example app listening on port 3000!');
-});
+window.alert("HOLA")
