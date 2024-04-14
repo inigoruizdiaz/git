2d452f3a0af4da307b27a6bbf1ed281078ee1a7f
inigoruizdiaz <inigoruiz@MacBook-Pro-de-Inigo.local>
2024-04-14 18:33:12 +0200

    Arreglo JS

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ app.use(express.static(_dirname + '/public'));
 app.get('/hola', (req, res) => 
 { res.send('ruta Hola');
 });
+
+
+app.get('/adios', (req, res) => 
+{ res.send('ruta adios');
+});
+
+app.listen(3000, () =>{
+    console.log('Example app listening on port 3000!');
+});
