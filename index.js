902e48579474e0ad116c502a16cb49c2ecf64492
inigoruizdiaz <inigoruiz@MacBook-Pro-de-Inigo.local>
2024-04-14 18:26:18 +0200

    arreglo del js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-window.alert("HOLA")
+
+const express = require('express');
+const app = express();
+
+app.use(express.static(_dirname + '/public'));
+
+app.get('/hola', (req, res) => 
+{ res.send('ruta Hola');
+});
+
+app.listen(3000, () =>{
+    console.log('Example app listening on port 3000!');
+});
