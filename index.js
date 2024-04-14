87b6b3860144a86fc3b54a04db551853424d3324
inigoruizdiaz <inigoruiz@MacBook-Pro-de-Inigo.local>
2024-04-14 18:41:52 +0200

    ruta hola añadida

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ app.use(express.static(_dirname + '/public'));
 app.get('/hola', (req, res) => 
 { res.send('ruta Hola');
 });
+app.get('/hola', (req, res) => 
+{ res.send('ruta Hola');
+});
 
 
 app.get('/adios', (req, res) => 
