56d32192b061f7eb8211e4b29db80fd533d45f82
inigoruizdiaz <inigo@efilm.online>
2024-04-15 09:08:32 +0200

    Subida del JS con las rutas actualizadas

--- index.js
+++ index.js
@@ -4,31 +4,17 @@ const app = express();
 
 app.use(express.static(_dirname + '/public'));
 
-app.get('/hola', (req, res) => 
-{ res.send('ruta Hola');
-});
-
-app.get('/adioos', (req, res) => 
-{ res.send('ruta adios');
-});
-
-app.get('/adioos', (req, res) => 
-{ res.send('ruta adios');
-});
-
-
-app.get('/adioos', (req, res) => 
-{ res.send('ruta adios');
-});
-
-app.get('/adioos', (req, res) => 
-{ res.send('ruta adios');
-});
-
 app.get('/registro', (req, res) => 
-{ res.send('registro');
+{ res.send('ruta registro');
 });
 
+app.get('/audiovisuales', (req, res) => 
+{ res.send('ruta audiovisuales');
+});
+
+app.get('/login', (req, res) => 
+{ res.send('ruta para logn');
+});
 
 app.get('/login', (req, res) => 
 { res.send('ruta de login nueva');
